9182c278961fb28b028d4cc94cd58d52683a67d1
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2019-07-21 14:31:42 +0530

    new form added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  hostName = '';
+  userName = '';
+  password = '';
   constructor() { }
 
   ngOnInit() {
+    console.log('startedddd')
+  }
+
+  saveController() {
+    alert("dsugfuydsgfyud")
   }
 
 }
