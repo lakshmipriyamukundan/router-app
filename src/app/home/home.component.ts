93e94b5e2c66d7de59ddbe5a1cc51c1ae92a274e
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2019-07-22 14:32:07 +0530

    component changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { ControllerService } from '../app.service';
+import { Component, OnInit } from "@angular/core";
+import { ControllerService } from "../app.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  hostName = '';
-  userName = '';
-  password = '';
+  hostName = "";
+  userName = "";
+  password = "";
+  tab1 = true;
+  tab2 = false;
+  homeShow = true;
 
-  constructor(private controllerService: ControllerService) { }
+  constructor(private controllerService: ControllerService) {}
 
   ngOnInit() {
-    console.log('startedddd')
+    console.log("startedddd");
   }
 
   saveController() {
-    alert("dsugfuydsgfyud")
+    alert("dsugfuydsgfyud");
     console.log(this);
-    this.controllerService.addResume(
-      {
-        hostName: this.hostName, 
-        userName: this.userName, 
+    this.controllerService
+      .addResume({
+        hostName: this.hostName,
+        userName: this.userName,
         password: this.password
       })
       .subscribe(
-      (data: any) => {
-        console.log(data);
-      },
-      error => {
-        console.log(error);
-      },
-    );
+        (data: any) => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
-
+  clickTab1() {
+    this.tab1 = true;
+    this.tab2 = false;
+    this.homeShow = true;
+  }
+  clickTab2() {
+    this.tab2 = true;
+    this.tab1 = false;
+    this.homeShow = false;
+  }
 }
