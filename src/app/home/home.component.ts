a16570765847107eac4778b8e15466dfaf3f4785
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2019-07-24 19:26:12 +0530

    lint chages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,40 @@
-import { Component, OnInit } from "@angular/core";
-import { ControllerService } from "../app.service";
+import { Component, OnInit } from '@angular/core';
+import { ControllerService } from '../app.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  hostName = "";
-  userName = "";
-  password = "";
+  hostName: string;
+  userName: string;
+  password: string;
   tab1 = true;
   tab2 = false;
   homeShow = true;
   sites = [
-    { id: "1", name: "order 1" },
-    { id: "2", name: "order 2" },
-    { id: "3", name: "order 3" },
-    { id: "4", name: "order 4" }
+    { id: '1', name: 'order 1' },
+    { id: '2', name: 'order 2' },
+    { id: '3', name: 'order 3' },
+    { id: '4', name: 'order 4' },
   ];
 
   constructor(private controllerService: ControllerService) {}
 
   ngOnInit() {
-    console.log("startedddd");
+    console.log('startedddd');
   }
 
   saveController() {
-    // alert("dsugfuydsgfyud");
+    // alert('dsugfuydsgfyud');
     console.log(this);
     this.controllerService
       .addResume({
         hostName: this.hostName,
         userName: this.userName,
-        password: this.password
+        password: this.password,
       })
       .subscribe(
         (data: any) => {
@@ -41,7 +42,7 @@ export class HomeComponent implements OnInit {
         },
         error => {
           console.log(error);
-        }
+        },
       );
   }
   clickTab1() {
